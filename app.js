e8ed5feebe0243d3151cbd6cb52212a7093663f2
nolzart <omarnazaret@gmail.com>
2020-10-06 13:55:31 -0400

    handling POST requests

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-const tours = JSON.parse(fs.readFileSync('dev-data/data/tours-simple.json'));
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
     console.log('im hereeee');
@@ -12,6 +14,23 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours,
         },
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        if (err) throw err;
+        res.status(201).send({
+            status: 'sucess',
+            data: {
+                tour: newTour,
+            },
+        });
     });
 });
 
