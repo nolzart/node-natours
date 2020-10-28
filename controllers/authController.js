257ac1c2a13fb8af232be3e6bc1a6701d10990fb
nolzart <omarnazaret@gmail.com>
2020-10-28 09:43:22 -0400

    user roles and permissions

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,7 @@ exports.signup = catchAsync(async (req, res, next) => {
         email: req.body.email,
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm,
+        role: req.body.role,
     });
 
     const token = signToken(newUser._id);
@@ -87,3 +88,14 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.user = currentUser;
     next();
 });
+
+exports.restrictTo = (...roles) => (req, res, next) => {
+    if (!roles.includes(req.user.role))
+        return next(
+            new AppError(
+                'You do not have permission to perform this action',
+                403
+            )
+        );
+    next();
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -23,6 +23,10 @@ router
     .route('/:tourId')
     .get(tourController.getTour)
     .patch(tourController.updateTour)
-    .delete(tourController.deleteTour);
+    .delete(
+        authController.protect,
+        authController.restrictTo('admin', 'lead-guide'),
+        tourController.deleteTour
+    );
 
 module.exports = router;
