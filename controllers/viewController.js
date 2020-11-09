97c8aaaa033be8a939e12bb0071ec6b7e0b00e26
nolzart <omarnazaret@gmail.com>
2020-11-09 10:34:54 -0400

    building tour page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -10,7 +10,15 @@ exports.getOverview = catchAsync(async (req, res) => {
     });
 });
 
-exports.getTour = (req, res) =>
+exports.getTour = catchAsync(async (req, res) => {
+    // Get data
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        fields: 'review rating user',
+    });
+
     res.status(200).render('tour', {
         title: 'The Forest Hiker Tour',
+        tour,
     });
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,6 @@ const viewController = require('../controllers/viewController');
 const router = express.Router();
 
 router.route('/').get(viewController.getOverview);
-router.route('/tour').get(viewController.getTour);
+router.route('/tour/:slug').get(viewController.getTour);
 
 module.exports = router;
