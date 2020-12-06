c8306c5d0c66be491f2f90f5c1a28e19e62c5f5b
nolzart <omarnazaret@gmail.com>
2020-12-06 13:24:33 -0400

    Prepared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,7 +9,7 @@ const stripe = Stripe(
 exports.getCheckoutSession = async tourId => {
     try {
         const session = await axios(
-            `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+            `/api/v1/bookings/checkout-session/${tourId}`
         );
         await stripe.redirectToCheckout({
             sessionId: session.data.session.id
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -7,8 +7,8 @@ export const updateSettings = async (data, type) => {
     try {
         const url =
             type === 'data'
-                ? 'http://127.0.0.1:3000/api/v1/users/updateMe'
-                : 'http://127.0.0.1:3000/api/v1/users/updateMyPassword';
+                ? '/api/v1/users/updateMe'
+                : '/api/v1/users/updateMyPassword';
 
         const res = await axios({
             method: 'PATCH',
