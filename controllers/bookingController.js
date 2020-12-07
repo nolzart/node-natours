3579a0489df6b5d6cbb90f179539a477b342bf9f
nolzart <omarnazaret@gmail.com>
2020-12-07 11:02:50 -0400

    Create booking checkout bug fixed

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -55,7 +55,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async session => {
     const tour = session.client_reference_id;
-    const user = (await User.findOne(session.customer_email)).id;
+    const user = (await User.findOne({ email: session.customer_email })).id;
     const price = session.display_items[0].amount / 100;
 
     await Booking.create({ tour, user, price });
