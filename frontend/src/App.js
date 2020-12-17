380efd839e4c0376d4a19b35349c6374fac78649
nolzart <omarnazaret@gmail.com>
2020-12-17 18:51:44 -0400

    converting styles from css to sass

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+// import Overview from './containers/Overview';
 
 function App() {
-    return <div className='App'>Hello World</div>;
+    return <div className='App'>{/* <Overview /> */}</div>;
 }
 
 export default App;
