2cdc5d8ae3de83dd3a1365d250e408d0f60a3cce
nolzart <omarnazaret@gmail.com>
2021-02-02 15:40:50 -0400

    unnecessary backend files removed

--- api/app.js
+++ api/app.js
@@ -16,7 +16,6 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
-// const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const bookingController = require('./controllers/bookingController');
 
@@ -86,7 +85,6 @@ app.use(
 app.use(compression());
 
 // ROUTES
-// app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -14,13 +14,19 @@ const signToken = id =>
 
 const createSendToken = (user, statusCode, req, res) => {
     const token = signToken(user._id);
-    res.cookie('jwt', token, {
+    const cookieOptions = {
         expires: new Date(
             Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
-        // secure: req.secure || req.headers['x-forwarded-proto'] === 'https', // only for production
-    });
+    };
+
+    if (process.env.NODE_ENV === 'production')
+        cookieOptions.secure =
+            req.secure || req.headers['x-forwarded-proto'] === 'https';
+
+    res.cookie('jwt', token, cookieOptions);
+
     user.password = undefined;
 
     res.status(statusCode).json({
